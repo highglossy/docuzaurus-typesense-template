eee99a98eae269c01f7bd9fc065caceb25ca0d6f
Nik <iam@niklucky.com>
2024-10-09 17:09:48 +0300

    Testing typesense

--- scripts/typesense_index.sh
+++ scripts/typesense_index.sh
@@ -3,4 +3,4 @@
 TYPESENSE_HOST=http://$HOST:$PORT
 config=$(cat typesense.config.json | jq ".index_name = \"${PROJECT}\"" | jq ".start_urls = [\"${TYPESENSE_HOST}\"]" | jq ".sitemap_urls = [\"${TYPESENSE_HOST}/sitemap.xml\"]" | jq -r tostring)
 echo $config
-docker run -it --env-file=./.env -e "CONFIG=$config" typesense/docsearch-scraper:0.9.1
+docker run --env-file=./.env -e "CONFIG=$config" typesense/docsearch-scraper:0.9.1
