aa86164f838c31c4f2ce81cc8d3b20e3f7f4cc43
Nik <iam@niklucky.com>
2024-10-09 16:49:53 +0300

    Changed bash to sh for pipeline

--- scripts/typesense_index.sh
+++ scripts/typesense_index.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 TYPESENSE_HOST=http://$HOST:$PORT
 config=$(cat typesense.config.json | jq ".index_name = \"${PROJECT}\"" | jq ".start_urls = [\"${TYPESENSE_HOST}\"]" | jq ".sitemap_urls = [\"${TYPESENSE_HOST}/sitemap.xml\"]" | jq -r tostring)
