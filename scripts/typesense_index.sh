0cfb92f2aa24c7a659d9ccbb50ab2e035ef468d8
Nik <iam@niklucky.com>
2024-10-10 01:57:37 +0300

    Fixed typesense url

--- scripts/typesense_index.sh
+++ scripts/typesense_index.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-config=$(cat typesense.config.json | jq ".index_name = \"${PROJECT}\"" | jq ".start_urls = [\"${PROJECT_URL}/docs\"]" | jq ".sitemap_urls = [\"${PROJECT_URL}/docs/sitemap.xml\"]" | jq -r tostring)
+config=$(cat typesense.config.json | jq ".index_name = \"${PROJECT}\"" | jq ".start_urls = [\"${PROJECT_URL}\"]" | jq ".sitemap_urls = [\"${PROJECT_URL}/sitemap.xml\"]" | jq -r tostring)
 echo $config
 docker run --env-file=./.env -e "CONFIG=$config" typesense/docsearch-scraper:0.9.1
